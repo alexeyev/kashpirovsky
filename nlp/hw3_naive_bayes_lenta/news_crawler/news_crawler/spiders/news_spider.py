2cca601116e7aaf0ff05f4f39f0a24610066a460
Roman <filiroman.tsu@gmail.com>
2014-04-26 13:00:00 +0400

    newsru.com spider

--- nlp/hw3_naive_bayes_lenta/news_crawler/news_crawler/spiders/news_spider.py
+++ nlp/hw3_naive_bayes_lenta/news_crawler/news_crawler/spiders/news_spider.py
@@ -12,44 +12,35 @@ from scrapy.item import Item
  
 class NewsSpider(CrawlSpider):
     name = "newsspider"
-    allowed_domains = ["lenta.ru"]
-    start_urls = ["http://lenta.ru/2014/01/01/"]
+    allowed_domains = ["auto.newsru.ru"]
+    start_urls = ["http://auto.newsru.ru/"]
     client = MongoClient()
     db = client['news_db']
-    collection = db['news_collection']
+    collection = db['news_collection4']
  
     rules = (
-    	Rule(SgmlLinkExtractor(allow=("\/\d{4}\/\d{2}\/\d{2}\/$"))),
-    	Rule(SgmlLinkExtractor(allow=("\/\d{4}\/\d{2}\/\d{2}\/\w+")), callback='parse_item'),
+    	Rule(SgmlLinkExtractor(allow=("\?page\=\d+"))),
+    	Rule(SgmlLinkExtractor(allow=("\/article\/\d{2}\w{3}\d{4}\/\w+")), callback='parse_item'),
     )
 
     def parse_item(self, response):
 
-    	#sel = Selector(response)
-        #return item
-	    hxs = Selector(response)
-	    #l = NewsLoader(NewsItem(), hxs)
-	    item_title = hxs.xpath('//*[@id=\"root\"]/nav[2]/div/div/div[1]/div/a[@class=\"item dark active\"]/text()').extract() #.encode('utf-8')
+		sel = Selector(response)
+		#return item
+		hxs = Selector(response)
+		ex_item = 'Авто'.decode('utf-8')
+		item_body = "".join(sel.xpath('//*[@id="text"]/p/text()').extract())
 
-	    if (len(item_title) > 0):
+		#if (final_item_topic.find(ex_item) == -1):
 
-		final_item_topic = item_title[0].strip()
-		item_body = "".join(hxs.xpath("//*[@id=\"root\"]/div[2]/div/div[1]/div[1]/div/div/div[@itemprop=\"articleBody\"]/p/text()").extract())
-
-		ex_item = 'Все'.decode('utf-8')
-
-		if (final_item_topic.find(ex_item) == -1):
-
-		    item = { "topic" : final_item_topic, 
-		    		 "body" : item_body }
+		item = { "topic" : ex_item, 
+				 "body" : item_body }
 
 
-		    self.collection.insert(item)
+		self.collection.insert(item)
 
-	    
+		#print hxs.xpath("//*[@id=\"root\"]/nav[2]/div/div/div[1]/div/a[@class=\"item dark active\"]/text()").extract()[0]
 
-	    #print hxs.xpath("//*[@id=\"root\"]/nav[2]/div/div/div[1]/div/a[@class=\"item dark active\"]/text()").extract()[0]
+		#print i.extract()[0]
 
-	    #print i.extract()[0]
-
-	    #l.add_value('url', response.url)
+		#l.add_value('url', response.url)
