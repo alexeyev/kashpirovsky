1e640e4eed24f3f0082d43fe4a2b11199dc78cbd
Roman <filiroman.tsu@gmail.com>
2014-05-06 15:37:49 +0400

    result set added

--- nlp/hw4_converter/xmlparser.py
+++ nlp/hw4_converter/xmlparser.py
@@ -5,5 +5,13 @@ tree = et.parse('and_good_table.xml')
 root = tree.getroot()
 ns = 'http://www.romip.ru/common/merged-results-QA-facts-extraction'
 for fact in root.iter('{%s}fact' % ns):
-    f.write(fact.attrib['firstText'].encode('utf-8') + '\t' + fact.attrib['cathegory'].encode('utf-8') +'\n')
-    f.write(fact.attrib['secondText'].encode('utf-8') + '\t' + fact.attrib['cathegory'].encode('utf-8') +'\n')
+	firstWords = fact.attrib['firstText'].encode('utf-8').split()
+	secondWords = fact.attrib['secondText'].encode('utf-8').split()
+
+	for fword in firstWords:
+		
+		f.write(fword + '\t' + fact.attrib['cathegory'].encode('utf-8') +'\n')
+
+	for sword in secondWords:
+
+		f.write(sword + '\t' + fact.attrib['cathegory'].encode('utf-8') +'\n')
