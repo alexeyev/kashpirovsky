5a6479d2b7980fada35d5af59112f1b1409c4f06
alexeyev <anton.m.alexeyev@gmail.com>
2014-05-19 01:14:55 +0400

    filtering

--- nlp/hw5_tweets_sental/predictor_extractor.py
+++ nlp/hw5_tweets_sental/predictor_extractor.py
@@ -4,6 +4,9 @@ from lxml import etree
 from lxml.etree import XMLParser
 from StringIO import StringIO
 from nltk import word_tokenize
+import re
+
+wordpattern = re.compile(u"[A-zА-Яа-я]+")
 
 xml = open("tweets/tweets_by_trend.xml", "r")
 tree = etree.parse(xml)
@@ -11,8 +14,10 @@ tree = etree.parse(xml)
 current_text = u""
 sent = -1
 
-def wordset(tweet):
-    return word_tokenize(tweet)
+def wordlist(tweet):
+    tokenized = word_tokenize(tweet)
+    filtered = [token.lower() for token in tokenized if wordpattern.match(token)]
+    return filtered
 
 for element in tree.getroot():
     for elem in element.iter():
@@ -20,5 +25,5 @@ for element in tree.getroot():
             current_text = elem.text
         elif elem.tag == 'sent':
             sent = int(elem.text)
-            print sent, " ".join(wordset(current_text))
+            print sent, " ".join(wordlist(current_text))
             
