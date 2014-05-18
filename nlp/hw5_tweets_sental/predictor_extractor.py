ce001669aeb00c451a6ff3f385790dfed1549436
alexeyev <anton.m.alexeyev@gmail.com>
2014-05-19 01:07:00 +0400

    parsing xml + tokenization

--- nlp/hw5_tweets_sental/predictor_extractor.py
+++ nlp/hw5_tweets_sental/predictor_extractor.py
@@ -1,7 +1,24 @@
 # coding:utf8
 
+from lxml import etree
 from lxml.etree import XMLParser
+from StringIO import StringIO
+from nltk import word_tokenize
 
-print "Hello, world!"
+xml = open("tweets/tweets_by_trend.xml", "r")
+tree = etree.parse(xml)
 
+current_text = u""
+sent = -1
 
+def wordset(tweet):
+    return word_tokenize(tweet)
+
+for element in tree.getroot():
+    for elem in element.iter():
+        if elem.tag == 'text':
+            current_text = elem.text
+        elif elem.tag == 'sent':
+            sent = int(elem.text)
+            print sent, " ".join(wordset(current_text))
+            
